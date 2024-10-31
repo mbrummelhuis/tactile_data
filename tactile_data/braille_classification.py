354f7de14d7190f8320caa7ab8dc93867f1d9013
Martijn Brummelhuis <mbrummelhuis@gmail.com>
2024-10-31 13:51:32 +0100

    Removed relative pathing in favour of absolute pathing

--- tactile_data/braille_classification.py
+++ tactile_data/braille_classification.py
@@ -2,5 +2,5 @@ import os
 
 BASE_PATH = os.path.dirname(__file__)
 
-BASE_DATA_PATH = os.path.join(BASE_PATH, '..', 'data', 'braille_classification')
-BASE_MODEL_PATH = os.path.join(BASE_PATH, '..', 'models', 'braille_classification')
+BASE_DATA_PATH = os.path.join(os.path.dirname(BASE_PATH), 'data', 'braille_classification')
+BASE_MODEL_PATH = os.path.join(os.path.dirname(BASE_PATH), 'models', 'braille_classification')
--- tactile_data/tactile_servo_control.py
+++ tactile_data/tactile_servo_control.py
@@ -2,6 +2,6 @@ import os
 
 BASE_PATH = os.path.dirname(__file__)
 
-BASE_DATA_PATH = os.path.join(BASE_PATH, '..', 'data', 'tactile_servo_control')
-BASE_MODEL_PATH = os.path.join(BASE_PATH, '..', 'models', 'tactile_servo_control')
-BASE_RUNS_PATH = os.path.join(BASE_PATH, '..', 'runs', 'tactile_servo_control')
+BASE_DATA_PATH = os.path.join(os.path.dirname(BASE_PATH), 'data', 'tactile_servo_control')
+BASE_MODEL_PATH = os.path.join(os.path.dirname(BASE_PATH), 'models', 'tactile_servo_control')
+BASE_RUNS_PATH = os.path.join(os.path.dirname(BASE_PATH), 'runs', 'tactile_servo_control')
